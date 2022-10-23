13748a8e281eaff35ed62cd9c7347f434da9367e
James Kirawan <jameskirawan@gmail.com>
2022-10-23 19:41:57 +0700

    add color pallet and font set up

--- src/styles/style.ts
+++ src/styles/style.ts
@@ -1,5 +1,28 @@
 import { createStitches } from "@stitches/react";
 
-const stitches = createStitches({});
+const stitches = createStitches({
+  theme: {
+    colors: {
+      primary0: "#000000",
+      primary1: "#150050",
+      primary2: "#3F0071",
+      primary3: "#FB2576",
+
+      white: "#FFFFFF",
+    },
+    fontWeights: {
+      regular: 400,
+      semiBold: 600,
+      bold: 700,
+    },
+    fontSizes: {
+      body1: 24,
+      body2: 22,
+      body3: 20,
+      body4: 18,
+      body5: 16,
+    },
+  },
+});
 
 export const { css } = stitches;
