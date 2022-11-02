83f0201623b0096d3df8e8abdae253e26bf1ea25
James Kirawan <jameskirawan@gmail.com>
2022-11-02 09:08:27 +0700

    refine dependecies when fetching

--- src/api-hooks/news/news.query.ts
+++ src/api-hooks/news/news.query.ts
@@ -6,10 +6,11 @@ import { ListReducers, useAppDispatch } from "../../redux/store";
 export default function useGetNews(props: NewsProps) {
   const { news, loading } = useSelector((state: ListReducers) => state.news);
   const dispatch = useAppDispatch();
+  const { query, everything, language, lastMonth, country } = props;
 
   React.useEffect(() => {
-    dispatch(getNewsApi(props));
-  }, [props.query]);
+    dispatch(getNewsApi({ query, everything, language, lastMonth, country }));
+  }, [country, dispatch, everything, language, lastMonth, query]);
 
   return { news, loading };
 }
